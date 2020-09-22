0bb37a1b858367f2f9f8349e5af410cbe5c88731
craig.matthew.smith@gmail.com <craig.matthew.smith@gmail.com>
2020-09-22 14:34:54 -0700

    website works for images

--- website/js/pgrad.js
+++ website/js/pgrad.js
@@ -8,7 +8,6 @@ $(function() {
     $(this).parent().parent().prev().html($(this).html()+'<span class="caret"></span>');    
   })
 
-
 //del_slp_all_init_KWMC-KSAC_gfs_10.png  
 //del_slp_all_init_KWMC-KSAC_gfs_4.png   
 
@@ -20,63 +19,87 @@ $(function() {
 //del_slp_all_model_KWMC-KSAC_current_10.png
 //del_slp_all_model_KWMC-KSAC_current_4.png
 
+//  function createPlotName(plotStn1, plotStn2, plotModel, plotTimeframe) {
+//    var plotName = "del_slp_all_init_"+plotStn1+"-"+plotStn2+"_"+plotModel+"_"+plotTimeframe+".png"
+//    console.log(plotName);
+//    return (plotName);
+//  }
 
-  function createPlotName(plotRegion,plotVariable,plotTimeframe) {
+
+  function createPlotName(plotStn1, plotStn2, plotModel, plotTimeframe) {
     //var plotName = plotVariable+"_"+plotRegion+"_stn_day_of_vs_time_"+plotTimeframe+".png"
     //ws_foothill_region_day_of_vs_time_current.png
-    var plotName = plotVariable+"_"+plotRegion+"_region_day_of_vs_time_"+plotTimeframe+".png"
+    if (plotModel == "all_model") {
+        var plotName = "del_slp_all_model_"+plotStn1+"-"+plotStn2+"_current_"+plotTimeframe+".png"
+    } else {
+        var plotName = "del_slp_all_init_"+plotStn1+"-"+plotStn2+"_"+plotModel+"_"+plotTimeframe+".png"   
+    }
     console.log(plotName);
     return (plotName);
   }
 
-  function createPlotText(plotRegionText,plotVariableText,plotTimeframeText) {
+  function createPlotText(plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText) {
     //var plotText = "Forecast "+plotVariableText+" at "+plotRegionText+" for "+plotTimeframeText
-    var plotText = "Forecast "+plotVariableText+" at "+plotRegionText+" stations for "+plotTimeframeText
+    if (plotModel == "all_model") {
+        var plotText = "Forecast pressure difference, "+plotStn1Text+" - "+plotStn2Text+", all models most recent init"
+    } else {
+        var plotText = "Forecast pressure difference, "+plotStn1Text+" - "+plotStn2Text+", "+plotModelText+" all init"
+    }
     console.log(plotText);
     return (plotText);
   }
 
-  function replaceImageAndText(plotRegion,plotRegionText,plotVariable,plotVariableText,plotTimeframe,plotTimeframeText) {
-    plotName = createPlotName(plotRegion,plotVariable,plotTimeframe);
-    plotText = createPlotText(plotRegionText,plotVariableText,plotTimeframeText);
-    $("#plot_selected").find('img').attr("src", "images/"+plotName);
+  function replaceImageAndText(plotStn1, plotStn2, plotModel, plotTimeframe, plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText) {
+    plotName = createPlotName(plotStn1, plotStn2, plotModel, plotTimeframe);
+    plotText = createPlotText(plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText);
+    $("#plot_selected").find('img').attr("src", "../images/"+plotName);
     //$("#plot_selected_text").text(plotText);
     $("#plot_text").text(plotText);
   }
 
-  var plotRegion = "foothill";
-  var plotVariable = "ws";
-  var plotTimeframe = "current";
+  var plotStn1 = "KWMC";
+  var plotStn2 = "KSAC";
+  var plotModel = "gfs";
+  var plotTimeframe = "10";
 
-  var plotRegionText = "foothill";
-  var plotVariableText = "wind speed";
-  var plotTimeframeText = "today";
+  var plotStn1Text = "Winnemucca";
+  var plotStn2Text = "Sacramento";
+  var plotModelText = "GFS";
+  var plotTimeframeText = "10 days";
      
-  plotName = createPlotName(plotRegion,plotVariable,plotTimeframe);
-  plotText = createPlotText(plotRegionText,plotVariableText,plotTimeframe);
+  plotName = createPlotName(plotStn1, plotStn2, plotModel, plotTimeframe);
+  plotText = createPlotText(plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText);
 
-  $("#variable-pills a").on('click', function (event) {
+  $("#stn1-pills a").on('click', function (event) {
     $(this).parent().toggleClass('open');
     $(this).parent().parent().find(".active").removeClass("active");
-    plotVariable     = $(this).data("variable");
-    plotVariableText = $(this).data("text");
-    replaceImageAndText(plotRegion,plotRegionText,plotVariable,plotVariableText,plotTimeframe,plotTimeframeText);
+    plotStn1     = $(this).data("value");
+    plotStn1Text = $(this).data("text");
+    replaceImageAndText(plotStn1, plotStn2, plotModel, plotTimeframe, plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText);
   });
 
-  $("#region-pills a").on('click', function (event) {
+  $("#stn2-pills a").on('click', function (event) {
     $(this).parent().toggleClass('open');
     $(this).parent().parent().find(".active").removeClass("active");
-    plotRegion = $(this).data("region");
-    plotRegionText = $(this).data("text");
-    replaceImageAndText(plotRegion,plotRegionText,plotVariable,plotVariableText,plotTimeframe,plotTimeframeText);
+    plotStn2     = $(this).data("value");
+    plotStn2Text = $(this).data("text");
+    replaceImageAndText(plotStn1, plotStn2, plotModel, plotTimeframe, plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText);
+  });
+
+  $("#model-pills a").on('click', function (event) {
+    $(this).parent().toggleClass('open');
+    $(this).parent().parent().find(".active").removeClass("active");
+    plotModel = $(this).data("value");
+    plotModelText = $(this).data("text");
+    replaceImageAndText(plotStn1, plotStn2, plotModel, plotTimeframe, plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText);
   });
 
   $("#timeframe-pills a").on('click', function (event) {
     $(this).parent().toggleClass('open');
     $(this).parent().parent().find(".active").removeClass("active");
-    plotTimeframe = $(this).data("timeframe");
+    plotTimeframe = $(this).data("value");
     plotTimeframeText = $(this).data("text");
-    replaceImageAndText(plotRegion,plotRegionText,plotVariable,plotVariableText,plotTimeframe,plotTimeframeText);
+    replaceImageAndText(plotStn1, plotStn2, plotModel, plotTimeframe, plotStn1Text, plotStn2Text, plotModelText, plotTimeframeText);
   });
 
 });
